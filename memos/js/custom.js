1a469086fa62ba437e3a1d8e6cda648536e0b980
TeacherDu <46226528+penndu@users.noreply.github.com>
2026-07-09 15:08:20 +0000

    a

--- memos/js/custom.js
+++
@@ -1,6 +0,0 @@
-// 只保留切换按钮点击调用原生逻辑，删除全部你写的darkmode代码
-const themeToggle = document.querySelector(".theme-toggle");
-themeToggle.addEventListener("click", () => {
-  // Stellar 全局暴露的主题切换函数
-  window.stellar.toggleTheme();
-});
